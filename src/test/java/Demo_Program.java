1ba349370a31a759a21100f098a07618e1c2a43b
bredlin-jose <bredlinjose.t@testyantra.com>
2023-06-05 15:37:31 +0530

    Framework

---
+++ src/test/java/Demo_Program.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo_Program {
+	@Test
+public void name() throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver=new ChromeDriver(options);
+		driver.get("https://www.google.com/");
+		Thread.sleep(3000);
+	
+}
+}
---
+++ src/test/java/git/practice/Bredlin.java
@@ -0,0 +1,9 @@
+package git.practice;
+
+public class Bredlin {
+
+	public static void main(String[] args) {
+		System.out.println("Hey! This is Bredlin");
+	}
+
+}
