e0631c84ab488cc2019e0930b36068bc07e811fc
bredlin-jose <bredlinjose.t@testyantra.com>
2023-06-15 17:13:04 +0530

    Commit

--- src/test/java/Demo_Program.java
+++ src/test/java/Demo_Program.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
